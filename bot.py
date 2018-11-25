16e8acaac23041e04f3d75f14468a8a0ffd770a8
Rodney Osodo <blackd0t@protonmail.com>
2018-11-25 16:47:47 +0300

    Adding bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telebot
+import time
+
+bot_token = '799826544:AAGU6sYuECKTDKz-16_Ywt3UCmBjL4n38Zo'
+
+bot = telebot.TeleBot(token=bot_token)
+
+def find_at(msg):
+    for text in msg:
+        if "@" in text:
+            return text
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome!")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "To use this bot, send it a username")
+
+@bot.message_handler(func=lambda msg: msg.text is not None and "@" in msg.text)
+def at_answer(message):
+    texts = message.text.split()
+    at_text = find_at(texts)
+
+    bot.reply_to(message, 'https://instagram.com/{}'.format(at_text[1:]))
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
